d7f95cc778e0761f1507e626c7358cb93bfff694
Ajay U <ajayudayasanu95@gmail.com>
2021-04-30 15:25:28 +0530

    added routing and few missed components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,18 @@
+import { LoginComponent } from './login/login.component';
+import { RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { environment } from 'src/environments/environment';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFireAuthModule } from '@angular/fire/auth';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { AngularFireAnalyticsModule } from '@angular/fire/analytics';
+import { HomeRoutingModule } from './app-routing.module';
+import { environment } from 'src/environments/environment';
+
+//  component imports
+import { AppComponent } from './app.component';
+import { ProductsComponent } from './products/products.component';
+import { HomeComponent } from './home/home.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 import { CheckOutComponent } from './check-out/check-out.component';
@@ -16,14 +22,57 @@ import { AdminProductsComponent } from './admin/admin-products/admin-products.co
 import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.component';
 
 @NgModule({
-  declarations: [AppComponent, NavBarComponent, ShoppingCartComponent, CheckOutComponent, OrderSuccessComponent, MyOrdersComponent, AdminProductsComponent, AdminOrdersComponent],
+  declarations: [
+    AppComponent,
+    NavBarComponent,
+    ShoppingCartComponent,
+    CheckOutComponent,
+    OrderSuccessComponent,
+    MyOrdersComponent,
+    AdminProductsComponent,
+    AdminOrdersComponent,
+  ],
   imports: [
     BrowserModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFirestoreModule,
     AngularFireAuthModule,
     AngularFireAnalyticsModule,
-    AppRoutingModule,
+    HomeRoutingModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        component: HomeComponent,
+      },
+      {
+        path: 'products',
+        component: ProductsComponent,
+      },
+      {
+        path: 'shopping-cart',
+        component: ShoppingCartComponent,
+      },
+      {
+        path: 'check-out',
+        component: CheckOutComponent,
+      },
+      {
+        path: 'order-success',
+        component: OrderSuccessComponent,
+      },
+      {
+        path: 'login',
+        component: LoginComponent,
+      },
+      {
+        path: 'admin/products',
+        component: AdminProductsComponent,
+      },
+      {
+        path: 'admin/orders',
+        component: AdminOrdersComponent,
+      },
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent],
