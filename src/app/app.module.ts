2d12015a886d4dec6b895eb9d99e26fa385c1310
Ajay U <ajayudayasanu95@gmail.com>
2021-04-30 14:16:19 +0530

    created components for public and admin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,15 @@ import { AngularFireAuthModule } from '@angular/fire/auth';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { AngularFireAnalyticsModule } from '@angular/fire/analytics';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { CheckOutComponent } from './check-out/check-out.component';
+import { OrderSuccessComponent } from './order-success/order-success.component';
+import { MyOrdersComponent } from './my-orders/my-orders.component';
+import { AdminProductsComponent } from './admin/admin-products/admin-products.component';
+import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.component';
 
 @NgModule({
-  declarations: [AppComponent, NavBarComponent],
+  declarations: [AppComponent, NavBarComponent, ShoppingCartComponent, CheckOutComponent, OrderSuccessComponent, MyOrdersComponent, AdminProductsComponent, AdminOrdersComponent],
   imports: [
     BrowserModule,
     AngularFireModule.initializeApp(environment.firebase),
